9eb41b779076df39d02267a48c32f6bed649ebde
Ryan S. Dancey <rsdancey@gmail.com>
2021-09-28 18:11:14 -0700

    Delete assets directory

--- assets/js/theme/common/gift-certificate-validator.js
+++
@@ -1,8 +0,0 @@
-export default function (cert) {
-    if (typeof cert !== 'string' || cert.length === 0) {
-        return false;
-    }
-
-    // Add any custom gift certificate validation logic here
-    return true;
-}
