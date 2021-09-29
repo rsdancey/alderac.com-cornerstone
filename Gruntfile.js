732722ab6135e9e4d25b2f5b9fb5e104ea3788d0
Ryan S. Dancey <rsdancey@gmail.com>
2021-09-28 18:13:08 -0700

    Delete Gruntfile.js

--- Gruntfile.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (grunt) {
-    require('time-grunt')(grunt);
-    require('load-grunt-config')(grunt);
-
-    grunt.loadNpmTasks('grunt-run');
-    grunt.loadNpmTasks('grunt-stylelint');
-    grunt.registerTask('default', ['eslint', 'svgstore']);
-};
