83b276af583fd3852cdff5d07e3e7b4e1c409fe8
Ryan Dancey <rdancey@alderac.com>
2021-09-28 18:15:10 -0700

    initial commit

---
+++ assets/js/theme/global/quick-view.js
@@ -0,0 +1,40 @@
+import 'foundation-sites/js/foundation/foundation';
+import 'foundation-sites/js/foundation/foundation.dropdown';
+import utils from '@bigcommerce/stencil-utils';
+import ProductDetails from '../common/product-details';
+import { defaultModal } from './modal';
+import 'slick-carousel';
+import { setCarouselState, onSlickCarouselChange, onUserCarouselChange } from '../common/carousel';
+
+export default function (context) {
+    const modal = defaultModal();
+
+    $('body').on('click', '.quickview', event => {
+        event.preventDefault();
+
+        const productId = $(event.currentTarget).data('productId');
+
+        modal.open({ size: 'large' });
+
+        utils.api.product.getById(productId, { template: 'products/quick-view' }, (err, response) => {
+            modal.updateContent(response);
+
+            modal.$content.find('.productView').addClass('productView--quickView');
+
+            const $carousel = modal.$content.find('[data-slick]');
+
+            if ($carousel.length) {
+                $carousel.on('init breakpoint swipe', setCarouselState);
+                $carousel.on('click', '.slick-arrow, .slick-dots', setCarouselState);
+
+                $carousel.on('init afterChange', (e, carouselObj) => onSlickCarouselChange(e, carouselObj, context));
+                $carousel.on('click', '.slick-arrow, .slick-dots', $carousel, e => onUserCarouselChange(e, context));
+                $carousel.on('swipe', (e, carouselObj) => onUserCarouselChange(e, context, carouselObj.$slider));
+
+                $carousel.slick();
+            }
+
+            return new ProductDetails(modal.$content.find('.quickView'), context);
+        });
+    });
+}
